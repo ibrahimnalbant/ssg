7d46146dcf38bf172e94996403559c2700fdb255
ibrahim nalbant <nalbantibrahim@yahoo.com>
2023-06-04 23:49:37 +0300

    ibr20230604-01

--- src/methodcreaationobjectcreation1/Calculator.java
+++ src/methodcreaationobjectcreation1/Calculator.java
@@ -3,18 +3,19 @@ package methodcreaationobjectcreation1;
 public class Calculator {
     // iki variable’in degerlerini Cikarma yapan  bir method olusturalim
 
-    public static void cikarma(int a, int b) {
-
+    public static void sayilar(int a, int b) {
+        System.out.println("iki sayi :" +a +" ve "+ b);
     }
-        System.out.println("iki sayinin farki :" + (a-b));
 
+    public static int cikarma( int a , int b) {
+        System.out.println("iki sayinin farki :" + (a-b));
+        return a - b;
     }
 
     public static void main(String[] args) {
-        cikarma(5,6)
+        sayilar(5,6);
+        cikarma(5,6);
 
-    }public static int cikarma( int a , int b){
-        return a-b;
-
+    }
 
 }
--- src/methodcreaationobjectcreation1/SchoolRunner.java
+++ src/methodcreaationobjectcreation1/SchoolRunner.java
@@ -1,8 +1,12 @@
 package methodcreaationobjectcreation1;
 
-public class SchoolRunner {
+public class SchoolRunner extends School{
 
     public static void main(String[] args) {
 
+        System.out.println("okul ismi = " + okulIsmiRun);
     }
+
+    static School school = new School();
+    static String okulIsmiRun = school.okulIsmi;
 }
